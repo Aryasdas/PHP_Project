52d41e2235e39bf491e8e1bbe7bc785f89d35950
Aryasdas <aryadas5555@gmail.com>
2025-02-24 22:17:32 +0530

    added navbar and modified admission details using bootstrap5

--- pages/ShowAdmissions.php
+++ pages/ShowAdmissions.php
@@ -1,77 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<link rel="stylesheet" href="assets/css/style.css">
-<title>Title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Title</title>
 </head>
 <body>
-    <p>checking !</p>
-    <iframe
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">name</th>
-            <th scope="col">email</th>
-            <th scope="col">address</th>
-            <th scope="col">PinCode</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Gender</th>
-            <th scope="col">State</th>
-            <th scope="col">Date of Birth</th>
-            <th scope="col">Last Passout Year</th>
-            <th scope="col">Password</th>
-            <th scope="col">Is Approved</th>
-            <th scope="col">Is Admin</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-            <th scope="col">Approve</th>
-            
-        </tr>
-    </thead>
-    <tbody>
-    
-<?php 
+     <!-- ADDED NAVBAR -->
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
 
-require '../config/dbconn.php';
-$sql = "SELECT * FROM users";
-$result = mysqli_query($conn, $sql);
-if($result){
-    while($row=mysqli_fetch_assoc($result)){
+    <!-- ADDED SCROLLING TABLE CONTAINER -->
+    <div class="scrolling-table">
+        <table class="table table-bordered border-primary">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">PinCode</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Gender</th>
+                    <th scope="col">State</th>
+                    <th scope="col">Date of Birth</th>
+                    <th scope="col">Last Passout Year</th>
+                    <th scope="col">Is Approved</th>
+                    <th scope="col">Is Admin</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php require '../config/dbconn.php'; $sql = "SELECT * FROM users"; $result = mysqli_query($conn, $sql); if ($result) { while ($row = mysqli_fetch_assoc($result)) { ?>
+                <tr>
+                <td><?php echo $row["Name"] ?></td>
+                    <td><?php echo $row["Email"] ?></td>
+                    <td><?php echo $row["Address"] ?></td>
+                    <td><?php echo $row["Pin"] ?></td>
+                    <td><?php echo $row["Phone"] ?></td>
+                    <td><?php echo $row["gender"] ?></td>
+                    <td><?php echo $row["state"] ?></td>
+                    <td><?php echo $row["dob"] ?></td>
+                    <td><?php echo $row["lyp"] ?></td>
+                    <td><?php echo $row["isapprove"] ?></td>
+                    <td><?php echo $row["isadmin"] ?></td>
+                    <td><button type="button" class="btn btn-info">Edit</button></td>
+                    <td><button type="button" class="btn btn-danger">Delete</button></td>
+                </tr>
+                <?php } } else { echo "Error: " . mysqli_error($conn); } ?>
+            </tbody>
+        </table>
+    </div>
 
-?>
-
-        <tr>
-            <td><?php echo $row["name"] ?></td>
-            <td><?php echo $row["email"] ?></td>
-            <td><?php echo $row["address"] ?></td>
-            <td><?php echo $row["pin"] ?></td>
-            <td><?php echo $row["Phone"] ?></td>
-            <td><?php echo $row["gender"] ?></td>
-            <td><?php echo $row["state"] ?></td>
-            <td><?php echo $row["dob"] ?></td>
-            <td><?php echo $row["lyp"] ?></td>
-            <td><?php echo $row["pass"] ?></td>
-            <td><?php echo $row["isapprove"] ?></td>
-            <td><?php echo $row["isadmin"] ?></td>
-            <td><button type="button" class="btn btn-info">Edit</button></td>
-            <td><button type="button" class="btn btn-danger">Delete</button></td>
-            <td><button type="button" class="btn btn-success">Delete</button></td>
-        </tr>
-
-<?php
-}
-}
-
-?>
-</tbody>
-</table>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
 </body>
 </html>
